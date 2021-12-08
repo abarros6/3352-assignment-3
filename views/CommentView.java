e0ab43068a63c36ef44b4cccb32f845d0a35166d
prabh-eng <pbhati4@uwo.ca>
2021-12-08 18:55:46 -0500

    Update CommentView.java

--- views/CommentView.java
+++ views/CommentView.java
@@ -1,8 +1,25 @@
 package views;
 
 public class CommentView extends models.Observer {
+
     private controllers.LessonsController controller;
     private models.Comment model;
+
+
+    public CommentView(models.Comment model, controllers.LessonsController controller) {
+        this.setModel(model);
+
+        this.setController(controller);
+    }
+
+    public void update(){
+        this.show();
+    }
+
+    public void onCommentUpdate(){
+        String newCommentInfo = "";
+        //this.controller.updateModel(newCommentInfo);
+    }
 
     public void show() {
 
@@ -13,12 +30,19 @@ public class CommentView extends models.Observer {
     }
 
     public void setModel(models.Comment model) {
+        if (this.model != null){
+            this.model.detach(this);
+        }
+
         this.model = model;
-        this.model.attach(this);
+        this.model.attach(this);    
     }
 
+    public controllers.LessonsController getController() {
+        return this.controller;
+    }
 
-    public void update(){
-        this.show();
+    public void setController(controllers.LessonsController controller) {
+        this.controller = controller;
     }
 }
