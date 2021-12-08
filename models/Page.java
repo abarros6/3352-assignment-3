eee83de0b406511b7286a5e63bb25bb9c3e317be
prabh-eng <pbhati4@uwo.ca>
2021-12-08 18:21:05 -0500

    Update Page.java

--- models/Page.java
+++ models/Page.java
@@ -1,7 +1,18 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Page {
     private Content content;
+    ArrayList<Observer> observer = new ArrayList<>();
+
+    public void attach(Observer o){
+        this.observer.add(o);
+    }
+
+    public void detach(Observer o){
+        this.observer.remove(o);
+    }
 
     public Content getPageContent() {
         return content;
